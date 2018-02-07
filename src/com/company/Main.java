a43a63422f7bedfc71d711b8312deabec929ef4b
PlamenDochev <plamkata_dochev@abv.bg>
2018-02-07 14:17:56 +0200

    making array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter N");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] matrix = new int[n];
+        for (int row = 0; row < n ; row++) {
+            matrix[row] = row * 5;
+            System.out.println(matrix[row]);
+        }
+
     }
 }
